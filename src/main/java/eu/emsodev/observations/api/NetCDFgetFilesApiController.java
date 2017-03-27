e2307070a3da35e81047fb382477cbb4f9475fcf
uapponi <umberto.apponi@gmail.com>
2017-03-27 12:30:05 +0200

    Another test

--- src/main/java/eu/emsodev/observations/api/NetCDFgetFilesApiController.java
+++ src/main/java/eu/emsodev/observations/api/NetCDFgetFilesApiController.java
@@ -69,7 +69,7 @@ public class NetCDFgetFilesApiController implements NetCDFgetFilesApi {
 			 obj = new JSONObject(response);
 			//inizio
 			 result= obj.getJSONObject("results");
-			ob = result.getString("metric");  
+			ob = obj.getString("metric");  
 		 //fine   
 			
 		} catch (JSONException e) {
