ac24b32d6c507f6706d5170598d8b12f231b51fd
leandro <leandro.lombardo@eng.it>
2017-03-28 14:38:30 +0200

    Test multi calls

--- src/main/java/eu/emsodev/observations/api/ODVgetFilesApiController.java
+++ src/main/java/eu/emsodev/observations/api/ODVgetFilesApiController.java
@@ -179,13 +179,16 @@ public class ODVgetFilesApiController implements ODVgetFilesApi {
 			boolean loop = true;
 			while (loop) {
 				if (sparkResult == null ) {
+					System.out.println("Inside loop result = " + sparkResult + " flag value = " + loop);
 					continue;
 				}else{
 					loop = false;
+					System.out.println("Inside loop result = " + sparkResult + " flag value = " + loop);
 				}
 			}
+			System.out.println("Exit loop = " + sparkResult + " flag value = " + loop);
 		}
-		System.out.println("Statement executed. The ODV file with the following id is mande : " + sparkResult);
+		System.out.println("Statement executed with result ID : " + sparkResult);
 		
 		
 		String odvFilePath = odvFilesPathPart + sparkResult;
