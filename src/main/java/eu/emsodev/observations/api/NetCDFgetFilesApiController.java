ec15dd3993814f9f186eafaf2316d57e2b57a3b5
uapponi <umberto.apponi@gmail.com>
2017-04-05 19:24:01 +0200

    Test_2

--- src/main/java/eu/emsodev/observations/api/NetCDFgetFilesApiController.java
+++ src/main/java/eu/emsodev/observations/api/NetCDFgetFilesApiController.java
@@ -127,13 +127,13 @@ public class NetCDFgetFilesApiController implements NetCDFgetFilesApi {
 				  Files.copy(file, response.getOutputStream());
 				  response.getOutputStream().flush();
 				  //is = new FileInputStream ("./Umberto.nc");
-		/*		try {
+			try {
 					org.apache.commons.io.IOUtils.copy(is, response.getOutputStream());
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-*/
+
 			    //response.flushBuffer();
 			} catch (FileNotFoundException e) {
 				// TODO Auto-generated catch block
