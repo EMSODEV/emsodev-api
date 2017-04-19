a199c54fac94a8d1fcc52151a45394cc74e8264d
uapponi <umberto.apponi@gmail.com>
2017-04-19 11:32:42 +0200

    Test_4

--- src/main/java/eu/emsodev/observations/api/NetCDFgetFilesApiController.java
+++ src/main/java/eu/emsodev/observations/api/NetCDFgetFilesApiController.java
@@ -310,7 +310,7 @@ public class NetCDFgetFilesApiController implements NetCDFgetFilesApi {
 			*/			  
 			  
 			
-        return new ResponseEntity<String>(compositeUrl+Data_2_out, HttpStatus.OK);
+        return new ResponseEntity<String>(Data_2_out, HttpStatus.OK);
     }
 
 }
