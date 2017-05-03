d2dd47d8d0d2bffd8b08dc0f2e8f4dfbc453acbc
uapponi <umberto.apponi@gmail.com>
2017-05-03 16:56:32 +0200

    Test_without_TIME

--- src/main/java/eu/emsodev/observations/api/NetCDFgetFilesApiController.java
+++ src/main/java/eu/emsodev/observations/api/NetCDFgetFilesApiController.java
@@ -359,7 +359,7 @@ public class NetCDFgetFilesApiController implements NetCDFgetFilesApi {
 		writer.addGroupAttribute(null, new Attribute("lunghezza",(int)occurance ));
 		
 		//Scrivo le dimensioni standard for Oceansites
-		T=writer.addDimension(null, "TIME", (int)occurance); //nome della dimensione e grandezza sono dati da metodi in Acquire 
+		//T=writer.addDimension(null, "TIME", (int)occurance); //nome della dimensione e grandezza sono dati da metodi in Acquire 
 	    D=writer.addDimension(null, "DEPTH", 1);
 	    LA=writer.addDimension(null, "LATITUDE", 1);
 	    //LO=writer.addDimension(null, "LONGITUDE", 1);
