f157a4b6bb76fa03bc2046e36b4ceca8449f2906
uapponi <umberto.apponi@gmail.com>
2017-05-04 16:56:27 +0200

    Test

--- src/main/java/eu/emsodev/observations/api/NetCDFgetFilesApiController.java
+++ src/main/java/eu/emsodev/observations/api/NetCDFgetFilesApiController.java
@@ -603,10 +603,10 @@ public class NetCDFgetFilesApiController implements NetCDFgetFilesApi {
 			shape = v.getShape();
 			datas = new ArrayDouble.D1(shape[0]);
 			ima=datas.getIndex();
-			for(int i=0; i<shape[0]; i++){
-			f=arrayDps[i].split(":");
-			datas.setDouble(ima.set(i), Double.parseDouble(f[0]));
-			}
+			//for(int i=0; i<shape[0]; i++){
+			f=arrayDps[0].split(":");
+			datas.setDouble(ima.set(0), Double.parseDouble(f[0]));
+			//}
 			//datas.setDouble(ima.set(1), 900.00);
 		  	writer.write(v, datas);
 						
