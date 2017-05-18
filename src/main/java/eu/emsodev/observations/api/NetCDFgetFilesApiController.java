0c07c74cccb8a34470fa82c3553b5e0a154254f9
uapponi <umberto.apponi@gmail.com>
2017-05-18 12:30:03 +0200

    TEST

--- src/main/java/eu/emsodev/observations/api/NetCDFgetFilesApiController.java
+++ src/main/java/eu/emsodev/observations/api/NetCDFgetFilesApiController.java
@@ -649,9 +649,10 @@ public class NetCDFgetFilesApiController implements NetCDFgetFilesApi {
 			int hal=0;
 			for(String rep:jobjectDpsCleaned.split(",")){
 				f=rep.split(":");
-				datas_T.setString(ima.set(hal), f[0]);
 				hal++;
 			}
+			datas_T.setString(ima.set(0), "polly");
+			datas_T.setString(ima.set(1), "gino");
     		writer.write(v, datas_T);
 									
 			//Writing values into DEPTH variable 
