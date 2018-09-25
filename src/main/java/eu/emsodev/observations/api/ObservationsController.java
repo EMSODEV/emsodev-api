36787ab3dd748b7d1511a900af9da23f0873eeb5
leandro <leandro.lombardo@eng.it>
2018-09-25 16:40:28 +0200

    Removed test.utils from import

--- src/main/java/eu/emsodev/observations/api/ObservationsController.java
+++ src/main/java/eu/emsodev/observations/api/ObservationsController.java
@@ -31,7 +31,7 @@ import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.util.MetaAnnotationUtils;
+//import org.springframework.test.util.MetaAnnotationUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
